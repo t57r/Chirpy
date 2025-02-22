c058dfb443bc36b077b46bda2dd4c6de7bc6d78c
t57r <parshenkotaras@gmail.com>
2025-02-22 16:14:59 +0200

    Healthz method

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	req.Header.Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := http.Server{
 		Handler: mux,
